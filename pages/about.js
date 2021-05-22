d93f607636523833c09ad49a8e4f8834d4e14fd3
Joseph L Azzara <josephlazzara@Josephs-iMac.local>
2021-05-22 16:43:37 -0400

    First commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+const about =() => (
+        <div>
+            <h1><center>My Open Source Projects</center></h1>
+            <center><a href="https://github.com/joe-azzara">github.com/joe.azzara</a></center>
+        </div>
+
+)
+
+export default about
